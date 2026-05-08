f3326301481549dcbf8dd90bba5a0619b97411a8
devgaikwad2023-creator <devgaikwad2023@gmail.com>
2026-05-09 00:24:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+console.log("Genz Academy Loaded");
+
+// future search function
+function searchCourses() {
+  alert("Search working coming soon!");
+}
+
 console.log("Genz Student Academy Loaded Successfully");
 
 /* MOBILE MENU */
