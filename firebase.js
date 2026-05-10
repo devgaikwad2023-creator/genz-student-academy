52690f14c83042fff55ebafe9d6874c199a754cf
devgaikwad2023-creator <devgaikwad2023@gmail.com>
2026-05-10 17:47:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,26 +8,25 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Your Firebase Config
+// Firebase Config
 
 const firebaseConfig = {
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
   apiKey: "AIzaSyCeUJEVaKFNIgyroHAy_MFbx5Yv9POmUcQ",
+
   authDomain: "genz-student-academy.firebaseapp.com",
+
   projectId: "genz-student-academy",
+
   storageBucket: "genz-student-academy.firebasestorage.app",
+
   messagingSenderId: "358103651695",
+
   appId: "1:358103651695:web:b087ba14d01a6ba751049a",
+
   measurementId: "G-XJH4TKVQZW"
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+};
 
 // Initialize Firebase
 
@@ -37,47 +36,13 @@ const auth = getAuth(app);
 
 console.log("Firebase Connected Successfully");
 
-// LOGIN SYSTEM
-
-const loginForm = document.getElementById("login-form");
-
-if(loginForm){
-
-  loginForm.addEventListener("submit", (e)=>{
-
-    e.preventDefault();
-
-    const email = document.getElementById("email").value;
-
-    const password = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, password)
-
-    .then((userCredential)=>{
-
-      alert("Login Successful 😎");
-
-      window.location.href = "dashboard.html";
-
-    })
-
-    .catch((error)=>{
-
-      alert(error.message);
-
-    });
-
-  })
-
-}
-
 // SIGNUP SYSTEM
 
 const signupForm = document.getElementById("signup-form");
 
-if(signupForm){
+if (signupForm) {
 
-  signupForm.addEventListener("submit", (e)=>{
+  signupForm.addEventListener("submit", (e) => {
 
     e.preventDefault();
 
@@ -89,19 +54,55 @@ if(signupForm){
 
     createUserWithEmailAndPassword(auth, email, password)
 
-    .then((userCredential)=>{
+      .then((userCredential) => {
 
-      alert("Account Created Successfully 🚀");
+        alert("Account Created Successfully 🚀");
 
-      window.location.href = "login.html";
+        window.location.href = "login.html";
 
-    })
+      })
 
-    .catch((error)=>{
+      .catch((error) => {
 
-      alert(error.message);
+        alert(error.message);
 
-    });
+      });
+
+  });
+
+}
+
+// LOGIN SYSTEM
+
+const loginForm = document.getElementById("login-form");
+
+if (loginForm) {
+
+  loginForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const email =
+      document.getElementById("email").value;
+
+    const password =
+      document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+
+      .then((userCredential) => {
+
+        alert("Login Successful 😎");
+
+        window.location.href = "dashboard.html";
+
+      })
+
+      .catch((error) => {
+
+        alert(error.message);
+
+      });
 
   });
 
