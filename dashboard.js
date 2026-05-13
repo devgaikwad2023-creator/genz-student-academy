331704861cc0b706d35b5acaac58bbe6099210f0
devgaikwad2023-creator <devgaikwad2023@gmail.com>
2026-05-13 15:57:42 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+import { auth } from "./firebase.js";
+window.logoutUser = () => {
+    signOut(auth).then(() => {
+        alert("Logged Out!");
+        window.location.href = "index.html"; // Logout ke baad seedha home page
+    });
+};
